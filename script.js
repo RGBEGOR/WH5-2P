620160446eacea27996a9e9c225556f8eededff8
qwerty <qwerty@gmail.com>
2023-12-18 21:06:44 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,266 @@
+const headElem = document.getElementById("head");
+const buttonsElem = document.getElementById("buttons");
+const pagesElem = document.getElementById("pages");
+
+class Quiz
+{
+	constructor(type, questions, results)
+	{
+
+		this.type = type;
+
+		this.questions = questions;
+
+		this.results = results;
+
+		this.score = 0;
+
+		this.result = 0;
+
+		this.current = 0;
+	}
+
+	Click(index)
+	{
+		let value = this.questions[this.current].Click(index);
+		this.score += value;
+
+		let correct = -1;
+
+
+		if(value >= 1)
+		{
+			correct = index;
+		}
+		else
+		{
+
+			for(let i = 0; i < this.questions[this.current].answers.length; i++)
+			{
+				if(this.questions[this.current].answers[i].value >= 1)
+				{
+					correct = i;
+					break;
+				}
+			}
+		}
+
+		this.Next();
+
+		return correct;
+	}
+
+
+	Next()
+	{
+		this.current++;
+		
+		if(this.current >= this.questions.length) 
+		{
+			this.End();
+		}
+	}
+
+
+	End()
+	{
+		for(let i = 0; i < this.results.length; i++)
+		{
+			if(this.results[i].Check(this.score))
+			{
+				this.result = i;
+			}
+		}
+	}
+} 
+
+
+class Question 
+{
+	constructor(text, answers)
+	{
+		this.text = text; 
+		this.answers = answers; 
+	}
+
+	Click(index) 
+	{
+		return this.answers[index].value; 
+	}
+}
+
+
+class Answer 
+{
+	constructor(text, value) 
+	{
+		this.text = text; 
+		this.value = value; 
+	}
+}
+
+
+class Result 
+{
+	constructor(text, value)
+	{
+		this.text = text;
+		this.value = value;
+	}
+
+
+	Check(value)
+	{
+		if(this.value <= value)
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}
+	}
+}
+
+
+const results = 
+[
+	new Result("Ну это прям позор", 0),
+	new Result("Ну уже неплохо", 3),
+	new Result("Ваш уровень выше среднего", 4),
+	new Result("Поздравляем Вы успешно усвоили эту тему", 5)
+];
+
+const questions = 
+[
+	new Question(" В каком веке появился на свет Чингисхан?", 
+	[
+		new Answer('XV веке ', 0),
+		new Answer("XII веке ", 1),
+		new Answer("XI веке", 0),
+		new Answer("IX веке", 0)
+	]),
+	new Question("Против кого Тэмуджин предпринял свой первый самостоятельный военный поход?", 
+	[
+		new Answer('Татары', 1),
+		new Answer("Гуюны", 0),
+		new Answer("Венгры", 0),
+		new Answer("Китай", 0)
+	]),
+	new Question("В каком году Тэмуджин был провозглашен великим ханом?", 
+	[
+		new Answer('1227', 0),
+		new Answer("1137", 0),
+		new Answer("1111", 0),
+		new Answer("1206", 1)
+	]),
+	new Question("Какое самое важное достижение Чингисхана в годы правления?", 
+	[
+		new Answer('Войны и успехи завований ', 0),
+		new Answer("Единое сильное государство Монгольская Империя", 1),
+		new Answer("Много земель захватил", 0),
+		new Answer("Стал великим полкаводцем", 0)
+	]),
+	new Question("Какое имя было дано Чингисхану при рождении?", 
+	[
+		new Answer('Темуджин ', 1),
+		new Answer("Чингиз", 0),
+		new Answer("Чущпан", 0),
+		new Answer("нет ответа", 0)
+
+	]),
+];
+
+
+const quiz = new Quiz(1, questions, results);
+
+Update();
+
+
+function Update()
+{
+
+	if(quiz.current < quiz.questions.length) 
+	{
+
+		headElem.innerHTML = quiz.questions[quiz.current].text;
+
+
+		buttonsElem.innerHTML = "";
+
+
+		for(let i = 0; i < quiz.questions[quiz.current].answers.length; i++)
+		{
+			let btn = document.createElement("button");
+			btn.className = "button";
+
+			btn.innerHTML = quiz.questions[quiz.current].answers[i].text;
+
+			btn.setAttribute("index", i);
+
+			buttonsElem.appendChild(btn);
+		}
+		
+
+		pagesElem.innerHTML = (quiz.current + 1) + " / " + quiz.questions.length;
+
+		Init();
+	}
+	else
+	{
+
+		buttonsElem.innerHTML = "";
+		headElem.innerHTML = quiz.results[quiz.result].text;
+		pagesElem.innerHTML = "Ваши очки: " + quiz.score;
+	}
+}
+
+function Init()
+{
+
+	let btns = document.getElementsByClassName("button");
+
+	for(let i = 0; i < btns.length; i++)
+	{
+
+		btns[i].addEventListener("click", function (e) { Click(e.target.getAttribute("index")); });
+	}
+}
+
+function Click(index) 
+{
+
+	let correct = quiz.Click(index);
+
+
+	let btns = document.getElementsByClassName("button");
+
+
+	for(let i = 0; i < btns.length; i++)
+	{
+		btns[i].className = "button button_passive";
+	}
+
+	if(quiz.type == 1)
+	{
+		if(correct >= 0)
+		{
+			btns[correct].className = "button button_correct";
+		}
+
+		if(index != correct) 
+		{
+			btns[index].className = "button button_wrong";
+		} 
+	}
+	else
+	{
+
+		btns[index].className = "button button_correct";
+	}
+
+
+	setTimeout(Update, 1000);
+}
+
+
